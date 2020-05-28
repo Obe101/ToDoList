3d038eca4b349668a7f64b613b5517d87b20f797
Obe <o.bdondo@gmail.com>
2020-05-27 18:44:59 -0700

    Edited typscript functions an added validation summary

--- js/main.js
+++ js/main.js
@@ -3,9 +3,96 @@ var ToDoList = (function () {
     }
     return ToDoList;
 }());
+window.onload = function () {
+    var addbtn = getInputById("addBtn");
+    addbtn.onclick = addTask;
+};
+function addTask() {
+    if (isValid()) {
+        var task = getToDoTask();
+        displayToDoList(task);
+    }
+}
 function isValid() {
+    var valid = true;
+    var title = getInputById("title").value;
+    if (title == "") {
+        valid = false;
+        addErrorMsg("You must include a title.");
+    }
+    var currDate = new Date();
+    var dateString = getInputById("date").value;
+    var dueDate = new Date(dateString);
+    if (compareDate(dueDate, currDate) <= 0) {
+        valid = false;
+        addErrorMsg("You must choose a future due date.");
+    }
+    var importances = document.getElementById("importance").value;
+    if (importances == "0") {
+        valid = false;
+        addErrorMsg("Please select how soon task must be completed.");
+    }
+    return valid;
 }
-function getToDoList() {
+function getToDoTask() {
+    var task = new ToDoList();
+    var taskTitle = getInputById("title").value;
+    task.title = taskTitle;
+    var taskImportance = document.getElementById("importance").value;
+    task.importance = taskImportance;
+    var taskDueDate = getInputById("date").value;
+    task.dueDate = new Date(taskDueDate);
+    var completed = getInputById("complete");
+    task.isCompleted = completed.checked;
+    var taskNotes = getInputById("notes").value;
+    task.notes = taskNotes;
+    return task;
 }
-function displayToDoList(item) {
+function displayToDoList(task) {
+    var titleHeader = document.createElement("h3");
+    titleHeader.innerText = task.title;
+    var taskDate = document.createElement("p");
+    taskDate.innerText = task.dueDate.toDateString();
+    var taskNotes = document.createElement("p");
+    taskNotes.innerText = task.notes;
+    var taskDiv = document.createElement("div");
+    taskDiv.classList.add("todo");
+    if (task.isCompleted) {
+        taskDiv.classList.add("completed");
+    }
+    taskDiv.appendChild(titleHeader);
+    taskDiv.appendChild(taskDate);
+    taskDiv.appendChild(taskNotes);
+    if (task.isCompleted) {
+        var completedToDos = document.getElementById("completed-items");
+        completedToDos.appendChild(taskDiv);
+    }
+    else {
+        var incompletedToDos = document.getElementById("incompleted-items");
+        incompletedToDos.appendChild(taskDiv);
+    }
+}
+function getInputById(id) {
+    return document.getElementById(id);
+}
+function compareDate(date1, date2) {
+    var d1 = date1;
+    var d2 = date2;
+    var same = d1.getTime() === d2.getTime();
+    if (same)
+        return 0;
+    if (d1 > d2)
+        return 1;
+    if (d1 < d2)
+        return -1;
+}
+function addErrorMsg(errMsg) {
+    var errSummary = document.getElementById("validation-summary");
+    var errItem = document.createElement("li");
+    errItem.innerText = errMsg;
+    errSummary.appendChild(errItem);
+}
+function clearErrors() {
+    var errSummary = document.getElementById("validation-summary");
+    errSummary.innerText = "";
 }
--- ts/main.ts
+++ ts/main.ts
@@ -3,18 +3,128 @@ class ToDoList{
     dueDate:Date;
     isCompleted:boolean;
     notes:string;
-    importances:number;
+    importance:string;
+}
+window.onload = function () {
+    let addbtn = <HTMLElement>getInputById("addBtn");
+    addbtn.onclick = addTask;
 }
 
+function addTask() {
+    if (isValid()) {
+        let task = getToDoTask();
+        displayToDoList(task);
+    }
+}
 function isValid():boolean{
 
-}
+    let valid = true;
 
-function getToDoList():ToDoList {
+    let title = getInputById("title").value
+    if (title == "") {
+        valid = false;
+        addErrorMsg("You must include a title.");
+        
+    }
+    let currDate = new Date();
+    let dateString =  getInputById("date").value;
+    let dueDate = new Date(dateString);
+    if (compareDate(dueDate, currDate) <= 0 ){
+        valid = false;
+        addErrorMsg("You must choose a future due date.");
+        
+    }
+    let importances = (<HTMLOptionElement>document.getElementById("importance")).value;
+    if (importances == "0") {
+        valid = false;
+        addErrorMsg("Please select how soon task must be completed.")
+    }
     
+    return valid;
 }
 
-function displayToDoList(item:ToDoList):void {
+function getToDoTask():ToDoList {
+    let task = new ToDoList();
+
+    let taskTitle = getInputById("title").value;
+     task.title = taskTitle;
+
+    let taskImportance = (<HTMLOptionElement>document.getElementById("importance")).value;
+    task.importance = taskImportance;
+
+    let taskDueDate = getInputById("date").value;
+    task.dueDate = new Date(taskDueDate);
+
+    let completed = getInputById("complete");
+    task.isCompleted = completed.checked;
+
+    let taskNotes = getInputById("notes").value;
+    task.notes = taskNotes; 
+
+    return task;
+}
+
+function displayToDoList(task:ToDoList):void {
+
+    let titleHeader = document.createElement("h3");
+    titleHeader.innerText = task.title;
+
+    let taskDate = document.createElement("p");
+    taskDate.innerText = task.dueDate.toDateString();
+
+    let taskNotes = document.createElement("p");
+    taskNotes.innerText = task.notes;
+
+    let taskDiv = document.createElement("div");
+    taskDiv.classList.add("todo");
+    if (task.isCompleted) {
+        taskDiv.classList.add("completed");
+        
+    }
+
+    taskDiv.appendChild(titleHeader);
+    taskDiv.appendChild(taskDate);
+    taskDiv.appendChild(taskNotes);
     
+    if (task.isCompleted) {
+        let completedToDos = document.getElementById("completed-items");
+        completedToDos.appendChild(taskDiv);
+    } else {
+        let incompletedToDos = document.getElementById("incompleted-items");
+        incompletedToDos.appendChild(taskDiv)    
+    }
 }
 
+function getInputById(id:string): HTMLInputElement {
+    return <HTMLInputElement>document.getElementById(id);
+}
+
+function compareDate(date1: Date, date2: Date): number
+{
+  // With Date object we can compare dates them using the >, <, <= or >=.
+  // The ==, !=, ===, and !== operators require to use date.getTime(),
+  // so we need to create a new instance of Date with 'new Date()'
+  let d1 = date1; let d2 = date2;
+
+  // Check if the dates are equal
+  let same = d1.getTime() === d2.getTime();
+  if (same) return 0;
+
+  // Check if the first is greater than second
+  if (d1 > d2) return 1;
+ 
+  // Check if the first is less than second
+  if (d1 < d2) return -1;
+}
+
+function addErrorMsg(errMsg:string) {
+    let errSummary = document.getElementById("validation-summary");
+    let errItem = document.createElement("li");
+    errItem.innerText = errMsg;
+    errSummary.appendChild(errItem);
+}
+
+function clearErrors() {
+    let errSummary = document.getElementById("validation-summary");
+     errSummary.innerText = "";
+}
